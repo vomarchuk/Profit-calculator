4607d0463dfd8efedf7ea3b9b388bf6098f84187
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-04-21 22:26:26 +0200

    added layout details => type of deposit and select logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-import './sass/main.scss';
+import './main.scss';
+
+import './js/select';
---
+++ src/js/select.js
@@ -0,0 +1,13 @@
+const refs = {
+  select: document.querySelector('.type__select'),
+  selectCurrent: document.querySelector('.select-current'),
+  selectDrop: document.querySelector('.select-options'),
+};
+
+refs.select.addEventListener('click', e => {
+  refs.selectDrop.classList.toggle('is-open');
+  if (e.target.classList.value != 'select-item') return;
+  refs.selectCurrent.textContent = e.target.textContent;
+  console.log(e.target.classList.value);
+  console.log(refs.selectCurrent.textContent);
+});
