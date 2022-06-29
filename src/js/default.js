04da0faae1d338001ea47be74bf094b0da55d357
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-06-29 23:02:08 +0200

    finished logic localStorage

--- src/js/default.js
+++ src/js/default.js
@@ -3,6 +3,4 @@ import { DEFAULT } from './CONSTANTS';
 const resultOnInvestment = JSON.parse(localStorage.getItem('resultOnInvestment'));
 if (resultOnInvestment === null) {
   localStorage.setItem('resultOnInvestment', JSON.stringify(DEFAULT));
-
-  return;
 }
